863daa3274817b9cb5e34e3603200baaade6922b
Ahmed Emad <ahmedemad56565@gmail.com>
2023-10-21 17:14:14 +0300

    connect to mongo DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((conn) => {
+    console.log(`DB connected...`);
+  })
+  .catch((err) => {
+    console.log(`message: ${err.message}`);
+  });
 
 app.listen(3000, () => {
   console.log('Server is running at port 3000');
