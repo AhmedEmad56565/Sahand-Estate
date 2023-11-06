ee0ecd7a8b0e2ec647b1ffc9ac7a907c69bfacd6
Ahmed Emad <ahmedemad56565@gmail.com>
2023-11-07 01:11:57 +0200

    create update listing api

--- api/controllers/listingControllers.js
+++ api/controllers/listingControllers.js
@@ -26,3 +26,28 @@ export const deleteListing = asyncHandler(async (req, res) => {
     throw new Error(`Error deleting listing`);
   }
 });
+
+export const updateListing = asyncHandler(async (req, res) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (listing) {
+    listing.name = req.body.name || listing.name;
+    listing.description = req.body.description || listing.description;
+    listing.address = req.body.address || listing.address;
+    listing.regularPrice = req.body.regularPrice || listing.regularPrice;
+    listing.discountPrice = req.body.discountPrice || listing.discountPrice;
+    listing.bathrooms = req.body.bathrooms || listing.bathrooms;
+    listing.bedrooms = req.body.bedrooms || listing.bedrooms;
+    listing.furnished = req.body.furnished || listing.furnished;
+    listing.parking = req.body.parking || listing.parking;
+    listing.type = req.body.type || listing.type;
+    listing.offer = req.body.offer || listing.offer;
+    listing.imageUrls = req.body.imageUrls || listing.imageUrls;
+
+    const updatedListing = await listing.save();
+    res.status(200).json(updatedListing);
+  } else {
+    res.status(404);
+    throw new Error(`Error deleting listing`);
+  }
+});
--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from '../controllers/listingControllers.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post('/create', protect, createListing);
 router.delete('/delete/:id', protect, deleteListing);
+router.put('/update/:id', protect, updateListing);
 
 export default router;
